0497e1328f0c1a73b86bc8a9505a3989127e50eb
Olivier Huvelle <olivier.huvelle@gmail.com>
2026-09-16 14:15:39 +0200

    ADD_olhu_setup_test : vitest basic configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import path from 'node:path'
 
 // https://vite.dev/config/
@@ -9,5 +9,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    globals: true,
   },
 })
