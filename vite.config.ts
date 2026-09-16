286090efa09ae2c75d1e5ea330766806f0c3e5ec
Olivier Huvelle <olivier.huvelle@gmail.com>
2026-09-16 15:08:06 +0200

    chore(setup): configure project tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    globals: true,
+  },
 })
